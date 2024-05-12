8f70fd5a9c08f86a389d1a3c4dec2ca51f291ce5
biswasbittu <biswasbittu7415@yahoo.com>
2024-05-12 12:17:20 +0530

    home banner done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,7 +21,40 @@ const Home = () => {
         </div>
       </div>
       {/* cover icon section */}
-      <div className="bg-[#ECEAE3] h-60"> Icon section</div>
+      <div className="bg-[#ECEAE3] ">
+        <div className="flex items-center justify-center p-8 gap-12 container mx-auto  ">
+          {/* logo-1 */}
+          <div className="">
+            <img src="../../../public/icons/1.png" alt="" className="w-12" />
+            <h2 className="text-xl font-medium mt-3">Awesome Aroma</h2>
+            <p>
+              You will definitely be a fan of the design & aroma of your coffee
+            </p>
+          </div>
+          {/* logo-2 */}
+          <div className="">
+            <img src="../../../public/icons/2.png" alt="" className="w-12" />
+            <h2 className="text-xl font-medium mt-3">High Quality</h2>
+            <p>We served the coffee to you maintaining the best quality</p>
+          </div>
+          {/* logo-3 */}
+          <div className="">
+            <img src="../../../public/icons/3.png" alt="" className="w-12" />
+            <h2 className="text-xl font-medium mt-3">Pure Grades</h2>
+            <p>
+              The coffee is made of the green coffee beans which you will love
+            </p>
+          </div>
+          {/* logo-4 */}
+          <div className="">
+            <img src="../../../public/icons/4.png" alt="" className="w-12" />
+            <h2 className="text-xl font-medium mt-3">Proper Roasting</h2>
+            <p>
+              Your coffee is brewed by first roasting the green coffee beans
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
