b37e7eb9926498fb15f9ae6b06df85a6cfc5e0c5
biswasbittu <biswasbittu7415@yahoo.com>
2024-05-13 11:35:58 +0530

    add coffee form done

--- src/components/AddCoffee/Addcoffee.jsx
+++ src/components/AddCoffee/Addcoffee.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
 
 
 const Addcoffee = () => {
+
+  const handleAddCoffee = e=>{
+    e.preventDefault();
+    const form=e.target;
+    const name= form.name.value;
+    const quentity = form.quentity.value;
+    const supplier = form.supplier.value;
+    const test = form.test.value;
+    const category = form.category.value;
+    const details = form.details.value;
+     const photo=form.photo.value;
+     const newCoffee = {name,quentity,supplier,test,category,details,photo}
+
+console.log(newCoffee);
+
+  }
     return (
       <div className="">
+        <Link to="/">Back To Home</Link>
         <div className=" w-2/3 mx-auto bg-[#F4F3F0] rounded-lg shadow-md p-6 mt-10">
           <h1 className="text-2xl font-bold mb-4 text-center">Add Coffee</h1>
           <p className="text-center px-32">
@@ -11,12 +29,12 @@ const Addcoffee = () => {
             of using Lorem Ipsum is that it has a more-or-less normal
             distribution of letters, as opposed to using Content here.
           </p>
-          <form className="p-8">
-            <div className="">
+          <form onSubmit={handleAddCoffee} className="p-8">
+            
               <div className="grid grid-cols-2 gap-4">
                 <div className="mb-4">
                   <label className="block  font-medium text-gray-700 text-xl">
-                    Name
+                    Coffee Name
                   </label>
                   <input
                     type="text"
@@ -26,11 +44,11 @@ const Addcoffee = () => {
                 </div>
                 <div className="mb-4">
                   <label className="block  font-medium text-gray-700 text-xl">
-                    Chef
+                    Quentity
                   </label>
                   <input
                     type="text"
-                    name="chef"
+                    name="quentity"
                     className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                   />
                 </div>
@@ -80,20 +98,20 @@ const Addcoffee = () => {
                   Photo
                 </label>
                 <input
-                  type="file"
-                  name="category"
+                  type="url"
+                  name="photo"
                   className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
                 />
               </div>
               <div>
                 <button
                   type="submit"
-                  className="bg-[#D2B48C] font-semibold px-4 py-2 rounded-md hover:bg-[#c49e6c] w-full"
+                  className="bg-[#D2B48C] font-semibold px-4 py-2 border-2 border-[#331A15] rounded-md hover:bg-[#c49e6c] w-full"
                 >
                   Add Coffee
                 </button>
               </div>
-            </div>
+            
           </form>
         </div>
       </div>
